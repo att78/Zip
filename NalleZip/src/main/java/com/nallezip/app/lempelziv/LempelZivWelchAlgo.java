2f6129ac1aedf320acc8ebf252d98ca0140c7201
Auli Tallberg <auli.tallberg@gmail.com>
2020-05-27 16:28:17 +0300

    Lempel Ziv Welch aloitettu

--- NalleZip/src/main/java/com/nallezip/app/lempelziv/LempelZivWelchAlgo.java
+++ NalleZip/src/main/java/com/nallezip/app/lempelziv/LempelZivWelchAlgo.java
@@ -5,13 +5,63 @@
  */
 package com.nallezip.app.lempelziv;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 /**
+ * Luokka toteuttaa Lempel-Ziv-Welch-algoritmin, joka on yksi niistä Lempel
+ * Ziv-algoritmeina tunnetuista algoritmeista
  *
  * @author tallbera
  */
 public class LempelZivWelchAlgo {
+
+    private List<Integer> encoded = new ArrayList();
+    private HashMap<String, Integer> library = new HashMap();
+
+    public LempelZivWelchAlgo() {
+    }
+
+    public List<Integer> encodeString(String string) {
+        int length = string.length();
+        createLibrary(string);
+        fillLibrary(string);
+        return encoded;        
+    }
+
+    /**
+     * Luodaan library annetulle stringille.
+     *
+     * @param string
+     */
+    public void createLibrary(String string) {
+        for (int i = 0; i < string.length(); i++) {
+            library.put("" + (char) i, i);
+        }
+    }
     
+    public void fillLibrary(String string){
+        String a = "";
+        String b = "";
+        int size = 128;
+        for (int i = 0; i < string.length(); i++) {
+            b = string.substring(i, i + 1);
+            String ab = a + b;
+//       for(char ch : string.toCharArray()){
+//           String ach = a+ch;
+            if (library.containsKey(ab)) {
+                a = ab;
+            } else {
+                encoded.add(library.get(a));
+                library.put(ab,size++);
+                a= ""+b;
+            }            
+            if(!a.equals("")){
+               encoded.add(library.get(a));
+            }
+        }   
+    }
     
-    
-    
+
 }
